3e575b1ffa4fab7627a4aa61c07ef1100f064554
João Lucas <lgbr212@gmail.com>
2025-12-23 14:52:38 -0300

    feat(front/service): Adicionando função para fazer post

--- front_demo_bloco_de_notas/scripts/service.js
+++ front_demo_bloco_de_notas/scripts/service.js
@@ -8,9 +8,41 @@ export async function getAllNotes() {
     });
 
     const notesData = await getNotesResponse.json();
+
+    console.log("=== DEBUG ===");
+    console.log(`É array? ${Array.isArray(notesData)}`)
+    console.log(`Quantidade de dados: ${notesData.length}`);
     return notesData;
   } catch (err) {
     console.log(`Erro ao tentar fazer fetch de dados: ${err.message}`);
     return [{ message: "Não foi possível encontrar os dados" }];
+  }
+}
+
+export async function postNewNote(event) {
+  event.preventDefault();
+
+  const inputTitleValue = document.querySelector(".title-input").value;
+  const inputContentValue = document.querySelector(".textarea-content").value;
+
+  try {
+    const createNoteResponse = await fetch(`${url}/notes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: inputTitleValue,
+        content: inputContentValue,
+      }),
+    });
+
+    if (!createNoteResponse.ok) {
+      console.log("Algo deu errado.");
+    }
+
+    console.log("Nota enviada.");
+  } catch (err) {
+    console.log(`Erro ao enviar nota: ${err.message}`);
   }
 }
